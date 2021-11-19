4293bbb092a679fb472fda7a76eec6c9cedcdfc2
PixelBoii <priwer.kontakt@gmail.com>
2021-11-19 10:37:57 +0100

    feat: Add game primary colors to chart

--- pages/index.js
+++ pages/index.js
@@ -20,16 +20,15 @@ const sparklineChartOpts = {
     xaxis: {
         type: 'datetime'
     },
-    colors: ['#80158c']
 }
 
-function Game(name, link, chartData) {
+function Game(game, link, chartData) {
     return (
         <div className="bg-white rounded-md shadow flex flex-col justify-between" key={link}>
             <Link href={`/${link}`}>
                 <a>
                     <div className="px-5 pt-6 pb-2">
-                        <p className="text-gray-700 font-semibold text-2xl"> { name } </p>
+                        <p className="text-gray-700 font-semibold text-2xl"> { game.name } </p>
                     </div>
                 </a>
             </Link>
@@ -38,10 +37,13 @@ function Game(name, link, chartData) {
                 className="w-full"
                 height="100px"
                 type="area"
-                options={sparklineChartOpts}
+                options={{
+                    ...sparklineChartOpts,
+                    colors: [game.primary_color],
+                }}
                 series={[
                     {
-                        name,
+                        name: 'Player Count',
                         data: chartData.map(e => ({ x: new Date(e[0]), y: e[1] }))
                     }
                 ]}
@@ -61,7 +63,7 @@ export default function Home() {
 
             <div className="container max-5xl mx-auto px-2">
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 -mt-8">
-                    { Object.keys(Games).map(game => Game(Games[game].name, game, ChartData[game])) }
+                    { Object.keys(Games).map(game => Game(Games[game], game, ChartData[game])) }
                 </div>
             </div>
         </div>
