1428ada92047c9be21c5f3e8474dbf8c6aeed0fa
PixelBoii <priwer.kontakt@gmail.com>
2021-11-19 09:21:56 +0100

    feat: Center navbar items and increase padding

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,10 @@ import Games from '../public/game_info.json'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <div className="w-full sticky top-0 bg-white flex items-center z-10">
+      <div className="w-full sticky top-0 bg-white flex items-center justify-center z-10">
         <Link href="/">
           <a>
-            <div className="px-4 py-3 hover:bg-gray-50">
+            <div className="px-4 py-4 hover:bg-gray-50">
               <p className="text-gray-700 font-semibold"> All Games </p>
             </div>
           </a>
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         { Object.keys(Games).map(game => (
           <Link href={`/${game}`} key={game}>
             <a>
-              <div className="px-4 py-3 hover:bg-gray-50">
+              <div className="px-4 py-4 hover:bg-gray-50">
                 <p className="text-gray-700 font-semibold"> { Games[game].name } </p>
               </div>
             </a>
