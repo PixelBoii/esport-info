1a9ca5cb71dbe576a93702a5bd17e9d293162e47
PixelBoii <priwer.kontakt@gmail.com>
2021-11-29 13:07:31 +0100

    refactor: Persist views in local storage

--- pages/api/views/[game].js
+++ pages/api/views/[game].js
@@ -1,19 +1,26 @@
-let views = {};
+import fs from 'fs'
 
 export default function handler(req, res) {
+    let storage = { views: {} };
     let { game } = req.query;
+
+    if (fs.existsSync('../storage.json')) {
+        storage = JSON.parse(fs.readFileSync('../storage.json'));
+    }
 
     if (req.method === 'GET') {
         res.json({
             views: views[game] || 0
         });
     } else if (req.method === 'POST') {
-        if (!views[game]) {
-            views[game] = 0;
+        if (!storage.views[game]) {
+            storage.views[game] = 0;
         }
 
-        views[game]++;
+        storage.views[game]++;
 
         res.status(200).end();
+
+        fs.writeFile('../storage.json', JSON.stringify(storage));
     }
 }
